a731a0f5e780d29809003a315d3647701ad5aeeb
Chris Eadie <chris.eadie@outlook.com>
2020-06-02 10:19:40 +0100

    Create project and add Task model

---
+++ TaskList/Models/Task.swift
@@ -0,0 +1,12 @@
+//
+//  Task.swift
+//  TaskList
+//
+//  Created by Chris Eadie on 02/06/2020.
+//  Copyright © 2020 ChrisEadieDesigns. All rights reserved.
+//
+
+struct Task {
+    var name: String
+    var completed = false
+}
